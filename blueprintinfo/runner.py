da4f5cfc13c06c2ebb6e4e1a924953911d0082dc
Andrew Ittner <aji@rhymingpanda.com>
2023-04-22 09:08:13 -0700

    Add blueprint-raw command

--- blueprintinfo/runner.py
+++ blueprintinfo/runner.py
@@ -1,6 +1,7 @@
 """Blueprint info"""
 
 import json
+import logging
 from pathlib import Path
 
 import click
@@ -10,6 +11,8 @@ from draftsman.blueprintable import BlueprintBook, Blueprint
 
 import info
 
+## GLOBALS ##
+logger = None
 
 @click.group()
 def cli():
@@ -27,6 +30,31 @@ def blueprint_info(bptext:str, debug:bool=False):
     data = info.parse_text(data, debug)
     print()
     info.report_hierarchy(data)
+
+@cli.command(help="'Raw' representation of the blueprint.")
+@click.option('--log-level', '-l',
+    type=click.Choice(('DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'), case_sensitive=False),
+    default="CRITICAL",
+    help='Select a log level.')
+@click.argument('bptext', type=click.Path(exists=True))
+def blueprint_raw(bptext:str, log_level:str='CRITICAL'):
+    _setup_logger(log_level)
+
+
+def _setup_logger(log_level:str='CRITICAL', debug:bool=False):
+    ## SETUP LOGGER ##
+    global logger
+    logger = logging.getLogger('bpi')
+    if debug:
+        print(f'{dry_run=}, {log_level=}, {debug=}')
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(log_level)
+    bf = logging.Formatter('[{name}] {message}', style='{')
+    handler = logging.StreamHandler()
+    handler.setFormatter(bf)
+    logger.addHandler(handler)
+    ## END LOGGER SETUP ##
 
 if __name__ == '__main__':
     cli()
